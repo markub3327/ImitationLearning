db72766449075d3d2a0f3e2a9839c731cf1a1867
Martin Kubovčík <markub3327@gmail.com>
2020-09-21 21:32:29 +0200

    Update agent.py

--- nets/agent.py
+++ nets/agent.py
@@ -4,37 +4,27 @@ import math
 from tensorflow.keras.layers import Input, Dense, Dropout, GRU
 from tensorflow.keras.models import Model
 from tensorflow.keras.optimizers import Adam
-from tensorflow.keras.callbacks import EarlyStopping
 from tensorflow.keras.models import load_model
 from tensorflow.keras.utils import plot_model
 from nets.base_model import BaseModel
-
-def decision_block(units, x, id):
-    l = Dense(units, activation='swish', use_bias=True, kernel_initializer='he_uniform', name='FC_'+id)(x)     # new activation function
-    l = Dropout(0.5, name='FC_'+id+'_dropout')(l)
-    return l
 
 class Agent:
     def __init__(self, lr=0.0001):
         self.lr = lr
 
-    def create(self, input_shape, hid=[32, 64]):
+    def create(self, input_shape, hid=32):
         state_inp = Input(shape=input_shape, name='image_input')
 
         # Base model (for transfer learning)
         l1, self._base_model = BaseModel(state_inp)
 
         # Memory blocks
-        l2 = GRU(hid[0], use_bias=True, name='gru_1')(l1)
-
-        # Decision blocks
-        l3 = l2
-        for i, x in enumerate(hid[1:]):
-            l3 = decision_block(x, l3, str(i))
-
+        l2 = GRU(hid, name='gru_1')(l1)
+        l3 = Dropout(0.5)(l2)
+        
         # output layer
-        steering_layer = Dense(1, activation='tanh', use_bias=True, name="steering")(l3)
-        acceleration_brake_layer = Dense(2, activation='sigmoid', use_bias=True, name="acceleration_brake")(l3)
+        steering_layer = Dense(1, activation='tanh', name="steering")(l3)
+        acceleration_brake_layer = Dense(2, activation='sigmoid', name="acceleration_brake")(l3)
 
         # create model
         self._model = Model(inputs=state_inp, outputs=[steering_layer, acceleration_brake_layer], name="agent")
@@ -44,8 +34,16 @@ class Agent:
         # show model
         self._model.summary()
 
-    def train(self, states, actions, epochs=100, batch_size=64, top_only=True, callbacks=[]):
-        if (top_only == False):
+    def top_only(self, a):
+        if a:
+            # Freeze the base model
+            self._base_model.trainable = False
+
+            self._model.compile(optimizer=Adam(learning_rate=self.lr, amsgrad=True), loss='mse')
+
+            # show model
+            self._model.summary()
+        else
             self._base_model.trainable = True
 
             # Let's take a look to see how many layers are in the base model
@@ -57,21 +55,14 @@ class Agent:
             # Freeze all the layers before the `fine_tune_at` layer
             for layer in self._base_model.layers[:fine_tune_at]:
                 layer.trainable =  False
-            self._model.compile(optimizer=Adam(learning_rate=self.lr/10.0, amsgrad=True), loss='mse')
-
-            # show model
-            self._model.summary()
-        else:
-            # Freeze the base model
-            self._base_model.trainable = False
-
             self._model.compile(optimizer=Adam(learning_rate=self.lr, amsgrad=True), loss='mse')
-
+            
             # show model
             self._model.summary()
-
+            
+    def train(self, states, actions, epochs=100, batch_size=64, callbacks=[]):
         self._model.fit(x=states, y={"steering": actions[:, 0], "acceleration_brake": actions[:, 1:]}, batch_size=batch_size, epochs=epochs, verbose=1, \
-             validation_split=0.20, callbacks=[EarlyStopping(monitor='val_loss', patience=16, verbose=1, restore_best_weights=True)]+callbacks)
+             validation_split=0.20, callbacks=callbacks)
     
     def save(self, path='save/model.h5'):
         self._model.save(path)
